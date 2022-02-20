a3e4bf241a907887709815fb4a6591f6b7a22c4a
markdmckenna <markdelaneymckenna@gmail.com>
2022-02-20 21:18:50 +0100

    Assignment 1
    Assignment 1: Assignment - Create Get Endpoint with Spring MVC

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerDto getCustomerById(UUID beerId) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .name("Mark McKenna")
+                .build();
+    }
+
+}
